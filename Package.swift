9aa09604b337b4f677f7b791df95bce533b1066b
Gaetano Matonti <gaetanomatonti@icloud.com>
2021-08-28 00:10:53 +0200

    Add Github Action for Build and Testing (#2)
    * Create swift.yml
    
    * Update Package.swift
    
    * Update Package.swift

--- Package.swift
+++ Package.swift
@@ -1,28 +1,36 @@
-// swift-tools-version:5.5
+// swift-tools-version:5.3
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
 
 let package = Package(
-    name: "Uno",
-    products: [
-        // Products define the executables and libraries a package produces, and make them visible to other packages.
-        .library(
-            name: "Uno",
-            targets: ["Uno"]),
-    ],
-    dependencies: [
-        // Dependencies declare other packages that this package depends on.
-        // .package(url: /* package url */, from: "1.0.0"),
-    ],
-    targets: [
-        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
-        // Targets can depend on other targets in this package, and on products in packages this package depends on.
-        .target(
-            name: "Uno",
-            dependencies: []),
-        .testTarget(
-            name: "UnoTests",
-            dependencies: ["Uno"]),
-    ]
+  name: "Uno",
+  platforms: [
+    .iOS(.v13), .macOS(.v10_15)
+  ],
+  products: [
+    .library(
+      name: "Uno",
+      targets: [
+        "Uno"
+      ]
+    ),
+  ],
+  dependencies: [
+    .package(url: "https://github.com/apple/swift-crypto.git", from: "1.1.0"),
+  ],
+  targets: [
+    .target(
+      name: "Uno",
+      dependencies: [
+        .product(name: "Crypto", package: "swift-crypto")
+      ]
+    ),
+    .testTarget(
+      name: "UnoTests",
+      dependencies: [
+        "Uno"
+      ]
+    ),
+  ]
 )
