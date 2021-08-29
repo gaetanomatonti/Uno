2b96b66c398d4fbddb7fa507e22c58ea4f8a9427
Gaetano Matonti <gaetanomatonti@icloud.com>
2021-08-29 17:27:19 +0200

    Add Time-based One Time Password Generator (#10)
    * Add logic for OTP generation and tests
    
    * Refactor code and add docs
    
    * Add docs and refactor
    
    * Fix README.md headers
    
    * Update Package.resolved
    
    * Add a new Secret wrapper object
    
    * Remove file name from headers
    
    * Refactor the HOTP generation and tests
    
    * Update docs
    
    * Rename OTP and HOTP with a "generator" name
    
    * Add TImeBasedGenerator and tests
    
    * Rename TimeBasedGenerator.swift to TimeBasedGeneratorTests.swift
    
    * Fix build error

---
+++ Sources/Uno/TimeBasedGenerator.swift
@@ -0,0 +1,83 @@
+//
+//  Uno
+//
+//  Created by Gaetano Matonti on 28/08/21.
+//
+
+#if canImport(CryptoKit)
+import CryptoKit
+#else
+import Crypto
+#endif
+
+import Foundation
+
+/// A type that represents a Time-based one-time password.
+public struct TimeBasedGenerator: AuthenticationCodeGenerator {
+  /// The possible errors thrown in the `TimeBasedGenerator`.
+  public enum Error: Swift.Error {
+    /// The provided timestep is not valid.
+    case timestepInvalid
+    
+    /// The time for counter computation is invalid.
+    case timeInvalid
+  }
+
+  // MARK: - Stored Properties
+  
+  public let secret: Secret
+  
+  public let codeLength: Int
+  
+  /// The period of validity of the authentication code expressed in seconds.
+  public let timestep: TimeInterval
+  
+  /// The underlying code-based generator to generate an HOTP.
+  private let counterBasedGenerator: CounterBasedGenerator
+  
+  // MARK: - Init
+  
+  public init(secret: Secret, codeLength: Int = 6, timestep: TimeInterval) {
+    self.secret = secret
+    self.codeLength = codeLength
+    self.timestep = timestep
+    self.counterBasedGenerator = CounterBasedGenerator(secret: secret, codeLength: codeLength)
+  }
+}
+
+// MARK: - Functions
+
+extension TimeBasedGenerator {
+  /// Generates a time-based OTP from the seconds interval.
+  /// - Parameter time: The `Date` representing the time to use for generation.
+  /// - Returns: A `String` representing the generated one-time password.
+  public func generate(from time: Date) throws -> String {
+    try generate(from: time.timeIntervalSince1970)
+  }
+  
+  /// Generates a time-based OTP from the seconds interval.
+  /// - Parameter secondsSince1970: The Unix epoch to use for generation.
+  /// - Returns: A `String` representing the generated one-time password.
+  public func generate(from secondsSince1970: TimeInterval) throws -> String {
+    guard secondsSince1970 >= 0 else {
+      throw Error.timeInvalid
+    }
+    
+    let counterFromSeconds = try counter(from: secondsSince1970)
+    return try counterBasedGenerator.generate(from: counterFromSeconds)
+  }
+  
+  /// Computes the counter factor from the seconds interval.
+  /// - Parameter secondsSince1970: The Unix epoch to use for generation.
+  /// - Returns: A `UInt64` representing the counter factor for HOTP generation.
+  func counter(from secondsSince1970: TimeInterval) throws -> UInt64 {
+    guard timestep >= 0 else {
+      throw Error.timestepInvalid
+    }
+    
+    // Round down and remove the fractional part.
+    let secondsSince1970 = floor(secondsSince1970)
+    let counter = floor(secondsSince1970 / timestep)
+    return UInt64(counter)
+  }
+}
